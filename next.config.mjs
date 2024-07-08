78ca2de011575cadc0436dba748ba1b7f0c58bbe
Mukul Sharma <sharma.mukul@rwit.io>
2024-07-08 17:30:58 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    RECAPTCHA_SITE_KEY: "6LeMyQoqAAAAAE3L5J7MbHPyVIjfKOuy_equUqBJ",
+    SITE_RECAPTCHA_SECRET: "6LeMyQoqAAAAABV1tuZyL3hXjIe2BU9yve-AjN5z",
+  },
+};
 
 export default nextConfig;
