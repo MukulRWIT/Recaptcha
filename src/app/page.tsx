b3fc61dd9253385857446a25b61a49c8a8c7e527
Mukul Sharma <sharma.mukul@rwit.io>
2024-07-08 16:55:19 +0530

    asf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,119 +1,94 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { createRef, useRef, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
-const Firstform = () => {
-  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    role: [],
-    message: "",
-    "form-name": "contact",
-    "g-recaptcha-response": "",
-  });
-  const handleChange = (e: any) => {
-    const { name, value, type, selectedOptions } = e.target;
-    if (type === "select-multiple") {
-      const values = Array.from(selectedOptions, (option: any) => option.value);
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: values,
-      }));
-    } else {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
+
+export default function Home() {
+  const recaptchaRef: any = useRef(null);
+  const siteKey: string = process.env.SITE_RECAPTCHA_KEY?.toString() as string;
+  const [captchaValue, setCaptchaValue] = useState(null);
+  const onChange = (val: any) => {
+    setCaptchaValue(val);
   };
-  function handleToken(token: any) {
-    setFormData((prevData) => ({
-      ...prevData,
-      ["g-recaptcha-response"]: token,
-    }));
-  }
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    try {
-      const res = await fetch("/__form.html", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData as any).toString(),
-      });
-      if (res.status === 200) {
-        alert("Submit");
-      } else {
-        alert("Error");
-      }
-    } catch (e) {
-      alert("Request failed");
-    }
+  const asyncScriptOnLoad = () => {
+    console.log("Google recaptcha loaded just fine");
   };
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    // formData["g-recaptcha-response"] = captchaValue;
+
+    fetch("/__form.html", {
+      method: "POST",
+      headers: { "Content-Type": "applicaation/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        ...formData,
+        "g-recaptcha-response": captchaValue,
+      } as any).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
   return (
-    <div>
-      <form name="contact" onSubmit={handleSubmit}>
+    <main className="flex flex-col gap-10 items-center justify-center h-[100vh]">
+      <h1 className="text-2xl ">FORM</h1>
+      <form
+        className="send_form flex flex-col gap-2 items-center justify-center "
+        name="contact"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <input type="hidden" name="form-name" value="contact" />
+        <p className="hidden">
+          <label>
+            Don’t fill this out if you’re human: <input name="bot-field" />
+          </label>
+        </p>
         <p>
           <label>
-            Your Name:
+            Your Name:{" "}
             <input
+              className="border-solid border-2 border-black "
               type="text"
               name="name"
-              className="border-2"
-              value={formData.name}
-              onChange={handleChange}
             />
           </label>
         </p>
         <p>
           <label>
-            Your Email:
+            Your Email:{" "}
             <input
+              className="border-solid border-2 border-black "
               type="email"
               name="email"
-              className="border-2"
-              value={formData.email}
-              onChange={handleChange}
             />
           </label>
         </p>
         <p>
           <label>
-            Your Role:{" "}
-            <select
-              name="role"
-              multiple
-              className="border-2"
-              value={formData.role}
-              onChange={handleChange}
-            >
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
+            Message:{" "}
             <textarea
+              className="border-solid border-2 border-black "
               name="message"
-              className="border-2"
-              value={formData.message}
-              onChange={handleChange}
-            />
+            ></textarea>
           </label>
         </p>
-        <ReCAPTCHA sitekey={siteKey || ""} onChange={handleToken} />
+        <ReCAPTCHA
+          ref={recaptchaRef}
+          size="normal"
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY! as string}
+          onChange={onChange}
+          asyncScriptOnLoad={asyncScriptOnLoad}
+        />
         <p>
-          <button
-            type="submit"
-            className="border bg-red-500 hover:bg-red-700 text-white w-[200px] h-[40px]"
-          >
+          <button className="border-solid border-2 border-black " type="submit">
             Send
           </button>
         </p>
       </form>
-    </div>
+    </main>
   );
-};
-export default Firstform;
+}
